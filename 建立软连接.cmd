2336f825cef29c818da795cf7d3138bda67e105a
funny050823 <funny_li@163.com>
2026-07-23 11:34:12 +0800

    调整脚本

--- 建立软连接.cmd
+++ 建立软连接.cmd
@@ -1,6 +1,23 @@
 @echo off
 
-mklink /d /j d:\QCBase\trunk\SourceCode\Tool\HDTools\KResourceReader\.claude\skills\Ani代码同步 %cd%\skills\Ani代码同步 
+set "desPath=d:\QCBase\trunk\SourceCode\Tool\HDTools\KResourceReader\.claude\skills"
 
+call :MakePathLink Ani代码同步
+call :MakePathLink kmsc代码同步
+call :MakePathLink krl代码同步
+call :MakePathLink tani代码同步
 pause
+goto :eof
 
+
+:MakePathLink
+    rem %1
+    if not exist "%cd%\skills\%1" (
+        mkdir "%cd%\skills\%1"
+    )
+    
+    if not exist "%desPath%\%1" (
+        mklink /d /j "%desPath%\%1" "%cd%\skills\%1"
+    )
+    goto :eof
+    
